86003d1cc0a48a7f2ef28eba6e5783a95ab3a0af
maiko.kuipers <maiko.kuipers@hva.nl>
2017-07-18 17:18:31 +0200

    Db migration

---
+++ database/migrations/2017_07_18_112448_add_user_data.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->string('token');
+            $table->string('remember_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            //
+        });
+    }
+}
