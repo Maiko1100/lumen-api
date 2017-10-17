b2d4b724af7792d2e0c72996ff0c00095588d7ec
maiko.kuipers <maiko.kuipers@hva.nl>
2017-10-17 14:55:04 +0200

    fixed email

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,15 +5,23 @@ namespace App\Http\Controllers;
 use App\Partner;
 use App\Question;
 use Laravel\Lumen\Routing\Controller as BaseController;
-
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\JsonResponse;
 
 class Controller extends BaseController
 {
     public function test() {
 
-        $question =  Question::get();
+        $data = [
+            'hendrik' => 'henk',
+            'email' => 'maiko@kcps.nl'
 
-        return new JsonResponse(['partner' => $question]);
+        ];
+
+        Mail::send('mails.testmail', $data, function ($message) use ($data) {
+            $message->to($data['email'], '')->subject('Do-not-reply:Afspraak');
+            $message->from('maiko@kcps.nl', 'Maiko');
+        });
+        return 'send';
     }
 }
