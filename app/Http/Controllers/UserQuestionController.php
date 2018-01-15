bb85c3c85d59bbec8e22b0bd96e39c458a949b33
Anthony Sitaram <anthony.sitaram@hva.nl>
2018-01-15 18:33:44 +0100

    fixed profile

--- app/Http/Controllers/UserQuestionController.php
+++ app/Http/Controllers/UserQuestionController.php
@@ -25,70 +25,149 @@ class UserQuestionController extends Controller
         $questionId = $request->input('id');
         $answer = $request->input('answer');
         $qpid = $request->input('qpid');
-        $userYear = UserYear::where("person_id", "=", $user->person_id)
-            ->where("year_id", "=", $year)->first();
-
-        $isProfile = Question::where("id", "=", $questionId)
-                ->first()
-                ->getGenre()
-                ->first()
-                ->isProfile == 1;
-
-        if ($isProfile) {
-            $existingProfileQuestion = $this->checkProfile($questionId, $qpid);
-
-            if (isset($existingProfileQuestion)) {
-                $existingProfileQuestion->question_answer = $answer;
-                $existingProfileQuestion->save();
-            } else {
-                $profileUserQuestion = new UserQuestion();
-                $profileUserQuestion->person_id = $user->person_id;
-                $profileUserQuestion->question_id = $questionId;
-                $profileUserQuestion->question_answer = $answer;
-                $profileUserQuestion->question_plus_id = $qpid;
-
-                $profileUserQuestion->save();
-            }
-        }
 
         $uq = null;
-        if (isset($qpid)) {
-            $existingQuestion = $this->checkPlus($questionId, $qpid, $userYear);
+        if (isset($year)) {
+            $userYear = UserYear::where("person_id", "=", $user->person_id)
+                ->where("year_id", "=", $year)->first();
 
-            if (isset($existingQuestion)) {
-                $existingQuestion->question_answer = $answer;
-                $existingQuestion->save();
+            $isProfile = Question::where("id", "=", $questionId)
+                    ->first()
+                    ->getGenre()
+                    ->first()
+                    ->isProfile == 1;
 
-                $uq = $existingQuestion;
+            if ($isProfile) {
+                $existingProfileQuestion = $this->checkProfile($questionId, $qpid);
+
+                if (isset($existingProfileQuestion)) {
+                    $existingProfileQuestion->question_answer = $answer;
+                    $existingProfileQuestion->save();
+                } else {
+                    $profileUserQuestion = new UserQuestion();
+                    $profileUserQuestion->person_id = $user->person_id;
+                    $profileUserQuestion->question_id = $questionId;
+                    $profileUserQuestion->question_answer = $answer;
+                    $profileUserQuestion->question_plus_id = $qpid;
+
+                    $profileUserQuestion->save();
+                }
+            }
+
+            if (isset($qpid)) {
+                $existingQuestion = $this->checkPlus($questionId, $qpid, $userYear);
+
+                if (isset($existingQuestion)) {
+                    $existingQuestion->question_answer = $answer;
+                    $existingQuestion->save();
+
+                    $uq = $existingQuestion;
+                } else {
+                    $userQuestion = new UserQuestion();
+                    $userQuestion->person_id = $user->person_id;
+                    $userQuestion->user_year_id = $userYear->id;
+                    $userQuestion->question_id = $questionId;
+                    $userQuestion->question_answer = $answer;
+                    $userQuestion->question_plus_id = $qpid;
+
+                    $userQuestion->save();
+
+                    $uq = $userQuestion;
+                }
             } else {
-                $userQuestion = new UserQuestion();
-                $userQuestion->person_id = $user->person_id;
-                $userQuestion->user_year_id = $userYear->id;
-                $userQuestion->question_id = $questionId;
-                $userQuestion->question_answer = $answer;
-                $userQuestion->question_plus_id = $qpid;
+                $existingQuestion = $this->check($userYear, $questionId);
+                if (isset($existingQuestion)) {
+                    $existingQuestion->question_answer = $answer;
+                    $existingQuestion->save();
 
-                $userQuestion->save();
+                    $uq = $existingQuestion;
+                } else {
+                    $userQuestion = new UserQuestion();
+                    $userQuestion->person_id = $user->person_id;
+                    $userQuestion->user_year_id = $userYear->id;
+                    $userQuestion->question_id = $questionId;
+                    $userQuestion->question_answer = $answer;
 
-                $uq = $userQuestion;
+                    $userQuestion->save();
+
+                    $uq = $userQuestion;
+                }
             }
         } else {
-            $existingQuestion = $this->check($userYear, $questionId);
-            if (isset($existingQuestion)) {
-                $existingQuestion->question_answer = $answer;
-                $existingQuestion->save();
+            $userYears = UserYear::where('status', '<', 3)
+                ->where('person_id', '=', $user->person_id)
+                ->get();
 
-                $uq = $existingQuestion;
+            if (isset($qpid)) {
+                $existingQuestion = $this->checkPlus($questionId, $qpid, NULL);
+
+                if (isset($existingQuestion)) {
+                    $existingQuestion->question_answer = $answer;
+                    $existingQuestion->save();
+
+                    $uq = $existingQuestion;
+                } else {
+                    $userQuestion = new UserQuestion();
+                    $userQuestion->person_id = $user->person_id;
+                    $userQuestion->question_id = $questionId;
+                    $userQuestion->question_answer = $answer;
+                    $userQuestion->question_plus_id = $qpid;
+
+                    $userQuestion->save();
+
+                    $uq = $userQuestion;
+                }
+
+                foreach($userYears as $userYear) {
+                    $existingQuestion = $this->checkPlus($questionId, $qpid, $userYear);
+
+                    if (isset($existingQuestion)) {
+                        $existingQuestion->question_answer = $answer;
+                        $existingQuestion->save();
+                    } else {
+                        $userQuestion = new UserQuestion();
+                        $userQuestion->person_id = $user->person_id;
+                        $userQuestion->user_year_id = $userYear->id;
+                        $userQuestion->question_id = $questionId;
+                        $userQuestion->question_answer = $answer;
+                        $userQuestion->question_plus_id = $qpid;
+
+                        $userQuestion->save();
+                    }
+                }
             } else {
-                $userQuestion = new UserQuestion();
-                $userQuestion->person_id = $user->person_id;
-                $userQuestion->user_year_id = $userYear->id;
-                $userQuestion->question_id = $questionId;
-                $userQuestion->question_answer = $answer;
+                $existingQuestion = $this->check(NULL, $questionId);
+                if (isset($existingQuestion)) {
+                    $existingQuestion->question_answer = $answer;
+                    $existingQuestion->save();
 
-                $userQuestion->save();
+                    $uq = $existingQuestion;
+                } else {
+                    $userQuestion = new UserQuestion();
+                    $userQuestion->person_id = $user->person_id;
+                    $userQuestion->question_id = $questionId;
+                    $userQuestion->question_answer = $answer;
 
-                $uq = $userQuestion;
+                    $userQuestion->save();
+
+                    $uq = $userQuestion;
+                }
+
+                foreach($userYears as $userYear) {
+                    $existingQuestion = $this->check($userYear, $questionId);
+                    if (isset($existingQuestion)) {
+                        $existingQuestion->question_answer = $answer;
+                        $existingQuestion->save();
+                    } else {
+                        $userQuestion = new UserQuestion();
+                        $userQuestion->person_id = $user->person_id;
+                        $userQuestion->user_year_id = $userYear->id;
+                        $userQuestion->question_id = $questionId;
+                        $userQuestion->question_answer = $answer;
+
+                        $userQuestion->save();
+                    }
+                }
             }
         }
 
@@ -98,15 +177,31 @@ class UserQuestionController extends Controller
 
     private function check($userYear, $questionId)
     {
-        return UserQuestion::where("user_year_id", "=", $userYear->id)->where("question_id", "=", $questionId)->first();
+        if (isset($userYear)) {
+            return UserQuestion::where("question_id", "=", $questionId)
+                ->where("user_year_id", "=", $userYear->id)
+                ->first();
+        } else {
+            return UserQuestion::where("question_id", "=", $questionId)
+                ->whereNull("user_year_id")
+                ->first();
+        }
+
     }
 
     private function checkPlus($questionId, $qpid, $userYear)
     {
-        return UserQuestion::where("question_id", "=", $questionId)
-            ->where("question_plus_id", "=", $qpid)
-            ->where("user_year_id", "=", $userYear->id)
-            ->first();
+        if (isset($userYear)) {
+            return UserQuestion::where("question_id", "=", $questionId)
+                ->where("question_plus_id", "=", $qpid)
+                ->where("user_year_id", "=", $userYear->id)
+                ->first();
+        } else {
+            return UserQuestion::where("question_id", "=", $questionId)
+                ->where("question_plus_id", "=", $qpid)
+                ->whereNull("user_year_id")
+                ->first();
+        }
     }
 
     private function checkProfile($questionId, $qpid)
