1a355ebf303fe9742ba19bd578374dd35762d2eb
maiko kuipers <maiko@MacBook-Pro-van-maiko.local>
2018-02-09 16:53:15 +0100

    updated email

--- resources/views/mails/statusMails/taxReturnUploaded.blade.php
+++ resources/views/mails/statusMails/taxReturnUploaded.blade.php
@@ -1,7 +1,14 @@
 Dear {{$name}},<br>
 <br>
-Your tax return is uploaded you can check it out when you login to the system.
+We hereby like to inform you that the Dutch tax authorities have imposed your {{$year}} preliminary tax assessment. <br>
+    Please find the copy of the tax assessment in the "tax return" section in your client portal. Go to the {{$year}} and under submission you will find the preliminary tax assessment. <br>
+The Dutch tax assessment is in line with the Dutch tax return filed on your behalf and no further action is required.<br>
 <br>
-Kind Regards,<br>
+    You can enter your client portal direct via this link.<br>
 <br>
-TTMTax
+    If you have any questions, please contact us via e-mail (info@ttmtax.nl) or call us via +31 (0)20 511009.<br>
+<br>
+    Enjoy the day<br>
+<br>
+    Best regards<br>
+     Team TTMTAX
