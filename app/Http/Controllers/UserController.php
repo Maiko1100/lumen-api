f40eb7dd96acb9567fb208f7c9f4b7ee019fec2c
Anthony Sitaram <anthony.sitaram@hva.nl>
2018-02-19 02:17:26 +0100

    changed hardcoded links for live

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -167,7 +167,7 @@ class UserController extends Controller
         $meeting->name = "test";
         $meeting->template="mails.userMails.passwordReset";
         $meeting->subject="Reset password";
-        $meeting->resetLink = env("BASE_URL_FRONT_END","http://test-ttmtax.kcps.nl/")."/reset?reset_token=".$token;
+        $meeting->resetLink = env("BASE_URL_FRONT_END","https://portal.ttmtax.nl/")."/reset?reset_token=".$token;
 
         MailController::sendMail($meeting);
 
